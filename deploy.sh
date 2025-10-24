eb1a1d48c68cfc53095de4ac064ee80256828dbe
Vivian Green <acorbett0528@gmail.com>
2025-10-24 13:03:43 -0700

    bump

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
-# deploy.sh
 
-# Use provided commit message or default to bump
-COMMIT_MSG="${1:-bump}"
+COMMIT_MSG="${1:-bump}" # COMMIT_MSG = arg1 ? arg1 : "bump"
 
-# Colors for output
 RED='\033[0;31m'
 GREEN='\033[1;32m'
+CYAN='\033[1;36m'
+GRAY='\033[0;37m'
 C='\033[0m'
 
 echo
 
+# build
+echo -e "${CYAN}    ----    Starting build...    ----    \n${C}"
 if ! python3 build_js.py; then
-    echo -e "${RED}\n\n     Build failed!\n\n${C}"
+    echo -e "${RED}\n\n    Build failed!\n\n${C}"
     exit 1
 fi
 
+
+
+# changes ? commit : no
+echo
 if git diff-index --quiet HEAD --; then
-    echo "No changes to commit."
+    echo -e "${GRAY}    ----    No changes to commit.    ----    ${C}"
 else
-    echo "Changes detected, committing..."
+    echo -e "${CYAN}    ----    Changes detected, committing...    ----    ${C}"
     git add . && \
     git commit -m "$COMMIT_MSG"
 fi
 
 # Push either way
-echo -e "\nPushing to GitHub..."
+echo -e "\n${CYAN}    ----    Pushing to GitHub...    ----    ${C}\n"
 if ! git push origin main; then
-    echo -e "${RED}\n\n     Failed to push to GitHub!${C}"
+    echo -e "\n\n${RED}    ----    Failed to push to GitHub!    ----    ${C}"
     exit 1
 fi
 
-echo -e "${GREEN}\n\n       Deployment completed successfully!\n\n${C}"
+echo -e "${GREEN}\n\n    ----    Deployment completed successfully! Haven gice done    ----    \n\n${C}"
+
+
+read -p "[press any to close] " -n1 -s
